eb50f85254bc67c26712948a33d79385e5cf2f5b
Alexander <d5enemy@gmail.com>
2018-12-26 11:58:51 +0200

    Ready simple application start with main page

--- src/main/java/com/alexxrw/websy/GreetingController.java
+++ src/main/java/com/alexxrw/websy/GreetingController.java
@@ -5,12 +5,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 @Controller
 public class GreetingController {
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+        return "greeting.mustache";
+    }
+
+    @GetMapping
+    public String main(Map<String, Object> model){
+        model.put("some", "Hello, lets code");
+        return "main";
     }
 }
