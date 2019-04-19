b95fa9057324a53e6002e93612620f32d64a2f8e
Alexander <d5enemy@gmail.com>
2019-04-19 15:24:34 +0300

    Change port for deploy on the new server

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,13 @@ echo "Copy files.."
 
 scp -i ~/.ssh/id_rsa \
     target/websy-1.0-SNAPSHOT.jar \
-    d5enemy@35.204.36.103:/home/d5enemy/
+    d5enemy@34.83.55.248:/home/d5enemy/
 
 echo "Restart server.."
 
-ssh -i ~/.ssh/id_rsa d5enemy@35.204.36.103 << EOF
+ssh -i ~/.ssh/id_rsa d5enemy@34.83.55.248 << EOF
 
+mkdir uploads
 pgrep java | xargs kill -9
 nohup java -jar websy-1.0-SNAPSHOT.jar > log.txt &
 
